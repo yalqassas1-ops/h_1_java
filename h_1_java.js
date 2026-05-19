1ca898c96b48d0a18cdcfd0f74c3ea53e36438e4
yalqassas1-ops <yalqassas1@smail.ucas.edu.ps>
2026-05-19 18:25:33 +0300

    h_1_java

---
+++ h_1_java.js
@@ -0,0 +1,32 @@
+let name="يوسف"
+let age="22"
+let hobby="البرمجه"
+console.log("الاسم:" +name);
+console.log("العمر:" +age);
+console.log("الهوايه المفضله:" +hobby);
+
+
+
+let length=prompt("ادخل الطول")
+let width=prompt("ادخل العرض")
+let area=length*width
+console.log(area)
+
+
+
+let quantity=100
+let price =2
+let total=quantity*price
+let tax =total*0.15
+let finalPrice =tax+total
+console.log(finalPrice)
+
+
+
+let num_=prompt("the number")
+if (num_%2==0) {
+    console.log("even"+ +num_)
+}
+else{
+    console.log("odd"+ +num_)
+}
